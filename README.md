1ad0823ff66bb66e5fa2638223537cec9b88c274
Ulong32 <na395955@gmail.com>
2023-04-23 16:32:11 +0900

    v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Lily-BDdownloader
+# LiliesNote
+
 A tool for celebrate Lily's birthday
+
+## About
+
+アサルトリリィRDFデータベース、[LuciaDB](https://github.com/Assault-Lily/LuciaDB)をデータソースとし、リリィの誕生日をiCal(.ics)形式でダウンロードできるツールです。
+
+## License
+
+LiliesNoteは[MIT ライセンス](https://opensource.org/licenses/MIT)です。
+ただし、このライセンスの適用範囲はこのリポジトリの内容に限られます。
+
+## How to Use
+
+<https://ulong32.github.io/LiliesNote> にアクセスし、日本語もしくは英語の「ダウンロード(Download)」を押すと「liliesbirthday.ics」というファイルがダウンロードされます。
+お使いのカレンダーアプリにインポートしてお使いください。
---
+++ script.js
@@ -0,0 +1,121 @@
+const queryHeader = `PREFIX schema: <http://schema.org/>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX lily: <https://luciadb.assaultlily.com/rdf/IRIs/lily_schema.ttl#>
+PREFIX lilyrdf: <https://luciadb.assaultlily.com/rdf/RDFs/detail/>`;
+
+const icsHeader = `BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//ulong32//ulong32//Lily-BDdownloader//JP
+CALSCALE:GREGORIAN
+BEGIN:VTIMEZONE
+TZID:Japan
+BEGIN:STANDARD
+DTSTART:19390101T000000
+TZOFFSETFROM:+0900
+TZOFFSETTO:+0900
+TZNAME:JST
+END:STANDARD
+END:VTIMEZONE`;
+
+function padding(number){
+    return number.toString().padStart(2,"0");
+}
+
+function download(lang) {
+    let server = "https://luciadb.assaultlily.com/sparql/query"
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST",server,false);
+
+    query = `
+SELECT ?name ?birthdate ?lgname ?lily
+WHERE {
+?lily a lily:Lily;
+schema:name ?name.
+FILTER(lang(?name)='${lang}')
+?lily schema:birthDate ?birthdate;
+}
+ORDER BY ?name`;
+
+    xhr.open("POST",server,false);
+    xhr.setRequestHeader("Content-Type", "application/sparql-query");
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.send(queryHeader + query);
+
+    let response = JSON.parse(xhr.responseText);
+    let resdata = response["results"]["bindings"];
+    let birthdata = [];
+    let icsdata = icsHeader;
+    let summary = "";
+
+    let date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let hour = date.getHours() + 1;
+    let minute = date.getMinutes();
+
+    let nextdate = [0,0,0]
+    //月末データ、視認性向上のため先頭は意味ないデータ
+    let monthend = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+    let i=0;
+    for(i=0;i<resdata.length;i++){
+
+        //めんどくさがって直接代入してるのでここでリセット
+        year = date.getFullYear();
+        // 名前、誕生月、誕生日
+        birthdata = [resdata[i]["name"]["value"], Number(resdata[i]["birthdate"]["value"].substring(2,4)), Number(resdata[i]["birthdate"]["value"].substring(5))];
+        //もう誕生日を過ぎてる場合は来年から
+        if(birthdata[1] < month || (birthdata[1] == month && birthdata[2] <= day)) {
+            year += 1;
+        }
+
+        //翌日の日付が月をまたぐ場合
+        if(birthdata[2] >= monthend[birthdata[1]]){
+            //年越し
+            if(birthdata[1] == 12){
+                nextdate = [year + 1,1,1]
+            }
+            nextdate[0] = year;
+            nextdate[1] = birthdata[1] + 1;
+            nextdate[2] = birthdata[2] - monthend[birthdata[1]] + 1;
+        }else {
+            nextdate[0] = year;
+            nextdate[1] = birthdata[1];
+            nextdate[2] = birthdata[2] + 1;
+        }
+        if(lang == "ja") {
+            summary = birthdata[0] + "の誕生日";
+        }else {
+            summary = birthdata[0] + "'s birthday";
+        }
+        LemonadeURL = resdata[i]["lily"]["value"].replace("https://luciadb.assaultlily.com/rdf/RDFs/detail/","https://lemonade.assaultlily.com/lily/");
+        icsdata += `
+BEGIN:VEVENT
+DTSTART;VALUE=DATE:${year.toString()}${padding(birthdata[1])}${padding(birthdata[2])}
+DTEND;VALUE=DATE:${nextdate[0].toString()}${padding(nextdate[1])}${padding(nextdate[2])}
+DTSTAMP:${year}${padding(month)}${padding(day)}T${padding(hour)}${padding(minute)}00
+RRULE:FREQ=YEARLY
+SUMMARY:${summary}
+URL;VALUE=URI:${LemonadeURL}
+END:VEVENT`;
+    }
+    document.getElementById("result").innerText = `${i}人のリリィの誕生日をエクスポートしました。`
+    
+    icsdata += "\nEND:VCALENDAR";
+    let outArea = document.getElementById("a");
+    outArea.value = icsdata;
+
+    //ダウンロード処理
+    const blob = new Blob([icsdata], {"type" : "text/calendar"});
+    const url = URL.createObjectURL(blob);
+
+    const anchor = document.createElement("a");
+    anchor.setAttribute("href",url);
+    anchor.setAttribute("download",'liliesbirthday.ics')
+    const mouseEvent = new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+        view: window
+    });
+    anchor.dispatchEvent(mouseEvent);
+}
