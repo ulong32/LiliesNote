c5dce9cf03198fdfe2665b353c98e4cd864d974b
Ulong32 <64057266+ulong32@users.noreply.github.com>
2023-06-13 22:44:47 +0900

    v4.0.0 Suzume

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const version = "v3.1.0 Louloudis";
+const version = "v4.0.0 Suzume";
 
 const queryHeader = `PREFIX schema: <http://schema.org/>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
