bcd2e94fdaa88ae1174c79bc949241349fe63e60
Ulong32 <64057266+ulong32@users.noreply.github.com>
2024-01-28 09:52:34 +0900

    フォーマット、不要なインポートを削除

--- src/lib/buildCalendar.ts
+++ src/lib/buildCalendar.ts
@@ -1,31 +1,31 @@
 import type { lilyBirthdayObject } from "./types";
 
-function pad(number :number) {
-    return String(number).padStart(2,"0");
+function pad(number: number) {
+    return String(number).padStart(2, "0");
 }
 
-function getNextDate(year :number, month :number, day :number) {
+function getNextDate(year: number, month: number, day: number) {
     const monthEnd = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    if(month === 12 && day === 31) {
+    if (month === 12 && day === 31) {
         return `${year + 1}0101`;
-    }else if(day + 1 > monthEnd[month]) {
+    } else if (day + 1 > monthEnd[month]) {
         return `${year}${pad(month + 1)}01`;
     } else {
         return `${year}${pad(month)}${pad(day + 1)}`;
     }
 }
 
-function getDescription(lilyBirthdayObject :lilyBirthdayObject) {
-    if( "lgname" in lilyBirthdayObject ){
+function getDescription(lilyBirthdayObject: lilyBirthdayObject) {
+    if ("lgname" in lilyBirthdayObject) {
         return `LG${lilyBirthdayObject.lgname!.value}所属、${lilyBirthdayObject.name.value}の誕生日です。`;
-    } else if( "garden" in lilyBirthdayObject && lilyBirthdayObject.type.value === "https://luciadb.assaultlily.com/rdf/IRIs/lily_schema.ttl#") {
+    } else if ("garden" in lilyBirthdayObject && lilyBirthdayObject.type.value === "https://luciadb.assaultlily.com/rdf/IRIs/lily_schema.ttl#") {
         return `${lilyBirthdayObject.garden!.value}の教導官、${lilyBirthdayObject.name.value}の誕生日です。`;
     } else {
         return `${lilyBirthdayObject.name.value}の誕生日です。`
     }
 }
 
-export function buildCalendar( lilyBirthdayObjects :lilyBirthdayObject[]) {
+export function buildCalendar(lilyBirthdayObjects: lilyBirthdayObject[]) {
     console.log(`Num: ${lilyBirthdayObjects.length}`)
     const currentDate = new Date();
     const currentDay = currentDate.getDate();
@@ -34,7 +34,7 @@ export function buildCalendar( lilyBirthdayObjects :lilyBirthdayObject[]) {
     const currentHour = currentDate.getHours();
     const currentMinute = currentDate.getMinutes();
 
-    let icsString :string = `BEGIN:VCALENDAR
+    let icsString: string = `BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//LuciaDB/ulong32/NONSGML LiliesNote ${APP_VERSION}//JA
 CALSCALE:GREGORIAN
@@ -43,7 +43,7 @@ X-LICENSE-COMMENT:このデータはLuciaDBから取得しています。ライ�
         icsString += `
 BEGIN:VEVENT
 DTSTART;VALUE=DATE:${currentYear}${entry.birthdate.value.replaceAll("-", "")}
-DTEND;VALUE=DATE:${getNextDate(currentYear, parseInt(entry.birthdate.value.slice(2,4)), parseInt(entry.birthdate.value.slice(5)))}
+DTEND;VALUE=DATE:${getNextDate(currentYear, parseInt(entry.birthdate.value.slice(2, 4)), parseInt(entry.birthdate.value.slice(5)))}
 DTSTAMP:${currentYear}${pad(currentMonth)}${pad(currentDay)}T${pad(currentHour)}${pad(currentMinute)}00
 RRULE:FREQ=YEARLY
 TRANSP:TRANSPARENT
