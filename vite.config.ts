f0b6b719bcce3db824c32fac826a656db979f69b
Ulong32 <64057266+ulong32@users.noreply.github.com>
2024-03-05 13:16:49 +0900

    PWA導入テスト

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()],
+	plugins: [sveltekit(), purgeCss(), SvelteKitPWA()],
 	define: {
 		APP_VERSION : JSON.stringify(process.env.npm_package_version)
 	}
